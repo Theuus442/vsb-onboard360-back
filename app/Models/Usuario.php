7a46edcca04a16efe14f6016629c9ec401809c11
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-25 13:16:27 -0300

    fix: modificação para utilização do Controller e Service

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
@@ -13,6 +14,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Usuario extends Authenticatable
 {
+    use HasFactory;
     protected $fillable = ['nome', 'email', 'senha', 'papel', 'departamento'];
 
     public function parceirosResponsaveis()
