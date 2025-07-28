84e0ce601ba39f7c153e9f06e699c7fbfd169703
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-28 11:41:49 -0300

    feat: adiciona checklistParceiroService para utilização do Controller

---
+++ app/Services/ChecklistParceiroService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\ChecklistParceiro;
+
+class ChecklistParceiroService
+{
+    public function listar($status = null)
+    {
+        $query = ChecklistParceiro::with(['parceiro', 'tarefaPadrao']);
+
+        if ($status) {
+            $query->where('status', $status);
+        } else {
+            $query->whereIn('status', ['pendente', 'em_andamento', 'concluido']);
+        }
+
+        return $query->orderBy('updated_at', 'desc')->paginate(10);
+    }
+
+    public function criar(array $dados)
+    {
+        return ChecklistParceiro::create($dados);
+    }
+
+    public function buscarPorId($id)
+    {
+        return ChecklistParceiro::with(['parceiro', 'tarefaPadrao', 'atualizadoPor'])->findOrFail($id);
+    }
+
+    public function atualizar($id, array $dados)
+    {
+        $checklist = ChecklistParceiro::findOrFail($id);
+        $checklist->update($dados);
+        return $checklist;
+    }
+
+    public function deletar($id)
+    {
+        $checklist = ChecklistParceiro::findOrFail($id);
+        $checklist->delete();
+        return true;
+    }
+}
