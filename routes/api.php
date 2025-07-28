b0cfe3d93a937794b2a98970460fc3eae1e21342
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-28 09:56:19 -0300

    fix: modificação para o api TarefaPadraoController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TarefaPadraoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 
 Route::apiResource('usuarios', UsuarioController::class);
+Route::apiResource('tarefas-padrao', TarefaPadraoController::class);
