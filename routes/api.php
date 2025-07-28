430135fb119235db2d6ee435e4d744390e49af29
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-28 11:17:53 -0300

    Feat: adiciona o DocumentoController nas rotas

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\UsuarioController;
 Route::apiResource('usuarios', UsuarioController::class);
 Route::apiResource('tarefas-padrao', TarefaPadraoController::class);
 Route::apiResource('parceiros', \App\Http\Controllers\ParceiroController::class);
+Route::apiResource('documentos', \App\Http\Controllers\DocumentoController::class);
