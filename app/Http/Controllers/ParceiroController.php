2ee65de80e3d411983a7bd89f0b47cfcc30ce015
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-28 10:44:32 -0300

    Refactor: refatoração do código para a utilização do Service

--- app/Http/Controllers/ParceiroController.php
+++ app/Http/Controllers/ParceiroController.php
@@ -2,49 +2,49 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Parceiro;
 use App\Http\Requests\ParceiroRequest;
 use App\Http\Requests\UpdateParceiroRequest;
+use App\Services\ParceiroService;
 
 class ParceiroController extends Controller
 {
+
+    protected $service;
+
+    public function __construct(ParceiroService $service)
+    {
+        $this->service = $service;
+    }
+
     public function index()
     {
-        $parceiros = Parceiro::with(['checklists', 'documentos'])->paginate(10);
+        $parceiros = $this->service->listar();
         return response()->json($parceiros);
     }
 
     public function store(ParceiroRequest $request)
     {
-        $validatedData = $request->validated();
-        $parceiro = Parceiro::create($validatedData);
+        $dados = $request->validated();
+        $parceiro = $this->service->criar($dados);
 
         return response()->json($parceiro, 201);
     }
 
     public function show($id)
     {
-        $parceiro = Parceiro::with(['checklists', 'documentos'])->findOrFail($id);
-
+        $parceiro = $this->service->buscarPorId($id);
         return response()->json($parceiro);
     }
 
     public function update(UpdateParceiroRequest $request, $id)
     {
-        $parceiro = Parceiro::findOrFail($id);
-        $validatedData = $request->validated();
-
-        $parceiro->update($validatedData);
-
+        $parceiro = $this->service->atualizar($id, $request->validated());
         return response()->json($parceiro);
     }
 
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        $parceiro = Parceiro::findOrFail($id);
-        $parceiro->status = 'inativo';
-        $parceiro->save();
-
+        $parceiro = $this->service->inativar($id);
         return response()->json(['message' => 'Parceiro marcado como inativo com sucesso!']);
     }
 }
