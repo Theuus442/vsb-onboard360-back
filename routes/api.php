c6dd15192cafa48d77a074d90764dcacb6583198
Matheus Alves <matheusalvesantana@hotmail.com>
2025-07-31 15:21:00 -0300

    feat(routes): rotas atualizadas para o consumo no front

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\ChecklistParceiroController;
 use App\Http\Controllers\DocumentoController;
 use App\Http\Controllers\AuthController;
 
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
 
 Route::post('/registrar', [AuthController::class, 'registrar']);
