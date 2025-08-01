41bf078b4aac9af03de275125d36a4139ef7f4c8
Matheus Alves <matheusalvesantana@hotmail.com>
2025-08-01 10:31:08 -0300

    Feat(kernel): middleware adicionado para o config do cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,11 +8,14 @@ class Kernel extends HttpKernel
 {
     protected $middlewareGroups = [
         'api' => [
+            \Illuminate\Http\Middleware\HandleCors::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
+
+
 
     protected $routeMiddleware = [
         'administrador' => \App\Http\Middleware\GarantirUsuarioAdministrador::class,
